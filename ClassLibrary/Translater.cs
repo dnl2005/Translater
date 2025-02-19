a544a542268f41bd90fe17de445faa62c58fc098
maggson <dieseverycingein@yandex.ru>
2025-02-20 00:01:33 +0300

    Обработчик ошибок пре-финал

--- ClassLibrary/Translater.cs
+++ ClassLibrary/Translater.cs
@@ -1,59 +1,69 @@
-﻿namespace ClassLibrary
+﻿using System.Globalization;
+
+namespace ClassLibrary
 {
     public class Translater
     {
-        public static readonly string invalid_p_ex = "InvalidPValueException";
-        public static readonly string invalid_q_ex = "InvalidQValueException";
-        public static readonly string invalid_input_n = "";
-        public static readonly string digit_out_of_P` = "NegativeNullSidesValueNgonException";
+        public static readonly string invalidNotationFromInputEx = "Ошибка: введены недопустимые символы в исходной системе счисления. См. справку";
+        public static readonly string invalidNotationFromValueEx = "Ошибка: введено неверное значение в исходной системе счисления. См. справку";
+        public static readonly string invalidNotationToInputEx = "Ошибка: введены недопустимые символы в конечной системе счисления. См. справку";
+        public static readonly string invalidNotationToValueEx = "Ошибка: введено неверное значение в исходной системе счисления. См. справку";
+        public static readonly string invalidAccuracyInputEx = "Ошибка: введены недопустимые символы в точности представления. См. справку";
+        public static readonly string invalidAccuracyValueEx = "Ошибка: введено недопустимое значение в точности представления. См. справку";
+        public static readonly string invalidNumberInputEx = "Ошибка: введены недопустимые значения в числе для перевода. См. справку";
+        public static readonly string DigitOutOfNotationToEx = "Ошибка: в выбранном числе присутствуют цифры вне выбранной исходной системы счисления";
+
 
         /// <summary>
-        /// Метод для обработки общей ошибки длины периметра
+        /// Метод для обработки ошибок входных данных.
+        /// Для проверки чисел на соответствие системе счисления и словарю используется сравнение по UniCode.
         /// </summary>
-        /// <param name="N"> Длина периметра </param>
-        /// <exception cref="Exception"> Ошибка неверной длины периметра</exception>
+        /// <param name="number">Вводимое для перевода число, строка</param>
+        /// <param name="notationFrom">Изначальная система счисления, строка</param>
+        /// <param name="notationTo">Конечная система счисления, строка</param>
+        /// <param name="accuracy">Точность представления для знаков после запятой, строка</param>
+        /// <exception cref="Exception">Возвращает ошибку, строка </exception>
         public static void ErrorDispatcher(string number, string notationFrom, string notationTo, string accuracy)
         {
-
-            if (!int.TryParse(notationFrom))
+            if (!int.TryParse(notationFrom, out int result))
             {
-                throw new Exception(invalidNotationFromInput)
+                throw new Exception(invalidNotationFromInputEx);
             }
-
-            if (notationFrom < 2 || notationFrom>36)
-            {
-                throw new Exception(invalidNotationFromValueEx)
-            }
-
-            char notationFrom = digits[int.Parse(notationFrom)]
-            if (!int.TryParse(notationTo))
-            {
-                throw new Exception(invalidNotationToInput)
-            }   
             
-            if (notationTo<2 || notationTo > 36)
+            if (int.Parse(notationFrom) < 2 || int.Parse(notationFrom)>36)
             {
-                throw new Exception(invalidNotationFromValueEx)
+                throw new Exception(invalidNotationFromValueEx);
             }
 
-            if (!int.TryParse(accuracy))
+            char notationFromUC = digits[int.Parse(notationFrom)]; //перевод изначальной системы счисления в UniCode
+            if (!int.TryParse(notationTo, out result))
             {
-                throw new Exception(invalidAccuracyInput)
+                throw new Exception(invalidNotationToInputEx);
+            }
+            
+            if (int.Parse(notationTo) <2 || int.Parse(notationTo) > 36)
+            {
+                throw new Exception(invalidNotationToValueEx);
             }
 
-            if (accuracy<1)
+            if (!int.TryParse(accuracy, out result))
             {
-                throw new Exception(invalidAccuracyValueEx)
+                throw new Exception(invalidAccuracyInputEx);
             }
 
-            for (int = 0; int < number.Length; int++)
+            if (int.Parse(accuracy) < 1)
+            {
+                throw new Exception(invalidAccuracyValueEx);
+            }
+
+            for (int i= 0;i < number.Length; i++)
                 {
 
-                    if (!digits.Contains(number[i]))
-                        throw new Exception(invalidNumberInput)
+                if (!digits.Contains(number[i]))
+                    throw new Exception(invalidNumberInputEx);
 
-                    if (number[i] > notationFrom)
-                        throw new Exception(DigitOutOfNotationTo)
+                    if (number[i] > notationFromUC)
+                    throw new Exception(DigitOutOfNotationToEx);
 
                 }
         }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,17 +1,18 @@
-using 
+using ClassLibrary;
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void ErrorDispatcher()
+        public void ErrorDispatcher_102_errorReturn()
         {
-            string testNUMBER1 = "101"`;
+            string testNUMBER1 = "102";
             string p = "2";
             string q = "10";
             string m = "4";
-            var result = Assert.ThrowsException<Exception>(()=>)
+            var result = Assert.ThrowsException<Exception>(() =>Translater.ErrorDispatcher(testNUMBER1,p,q,m));
+            Assert.AreEqual(Translater.invalidNotationFromValueEx, result.Message);
         }
     }
 }
