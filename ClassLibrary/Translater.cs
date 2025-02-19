48c4456bce8d455d7ae24f242ae956eab33de991
maggson <dieseverycingein@yandex.ru>
2025-02-19 23:05:08 +0300

    обработчик до тестов

--- ClassLibrary/Translater.cs
+++ ClassLibrary/Translater.cs
@@ -2,6 +2,60 @@
 {
     public class Translater
     {
+        public static readonly string invalid_p_ex = "InvalidPValueException";
+        public static readonly string invalid_q_ex = "InvalidQValueException";
+        public static readonly string invalid_input_n = "";
+        public static readonly string digit_out_of_P` = "NegativeNullSidesValueNgonException";
 
+        /// <summary>
+        /// Метод для обработки общей ошибки длины периметра
+        /// </summary>
+        /// <param name="N"> Длина периметра </param>
+        /// <exception cref="Exception"> Ошибка неверной длины периметра</exception>
+        public static void ErrorDispatcher(string number, string notationFrom, string notationTo, string accuracy)
+        {
+
+            if (!int.TryParse(notationFrom))
+            {
+                throw new Exception(invalidNotationFromInput)
+            }
+
+            if (notationFrom < 2 || notationFrom>36)
+            {
+                throw new Exception(invalidNotationFromValueEx)
+            }
+
+            char notationFrom = digits[int.Parse(notationFrom)]
+            if (!int.TryParse(notationTo))
+            {
+                throw new Exception(invalidNotationToInput)
+            }   
+            
+            if (notationTo<2 || notationTo > 36)
+            {
+                throw new Exception(invalidNotationFromValueEx)
+            }
+
+            if (!int.TryParse(accuracy))
+            {
+                throw new Exception(invalidAccuracyInput)
+            }
+
+            if (accuracy<1)
+            {
+                throw new Exception(invalidAccuracyValueEx)
+            }
+
+            for (int = 0; int < number.Length; int++)
+                {
+
+                    if (!digits.Contains(number[i]))
+                        throw new Exception(invalidNumberInput)
+
+                    if (number[i] > notationFrom)
+                        throw new Exception(DigitOutOfNotationTo)
+
+                }
+        }
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,12 +1,17 @@
+using 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ErrorDispatcher()
         {
-
+            string testNUMBER1 = "101"`;
+            string p = "2";
+            string q = "10";
+            string m = "4";
+            var result = Assert.ThrowsException<Exception>(()=>)
         }
     }
 }
