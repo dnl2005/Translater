dcbf50e3f424c9c0b84b9c105fa5be22f4aed5e5
Nikita Sergeevich <niba2017@mail.ru>
2025-02-20 14:54:29 +0300

    закончили фронт без справки

--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -10,17 +10,9 @@ namespace Interface
 
         private void result_Click(object sender, EventArgs e)
         {
-            /*string numbre = ;
-            int baseCh = ;
-            int changedBs = ;
-            int acc = ;*/
             resultNumbre.Text = Translater.ConvertDecToOther(Translater.ConvertOtherToDec(numbre.Text, int.Parse(baseToChange.Text), 
                 int.Parse(accuracy.Text)), int.Parse(changedBase.Text), int.Parse(accuracy.Text));//;
         }
 
-       /* private void textBox1_TextChanged(object sender, EventArgs e)
-        {   string numbre = textBox1.Text;
-            return numbre;
-        }*/
     }
 }
